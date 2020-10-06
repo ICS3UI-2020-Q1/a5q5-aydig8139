0c216a4e5cbb9d40627d870125e6bd6f9b397808
aydig8139 <aydig8139@wrdsb.ca>
2020-10-06 15:40:05 +0000

    finished question to print *s

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ * Draws a line of *s
+ * @author Rose
  */
 public class Main {
 
@@ -9,7 +11,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create new scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask user for a number and declare a variable to store it 
+    System.out.println("Please enter a number between 1 and 10");
+    int userNum = input.nextInt();
+
+    for(int count = 1; count <= userNum; count++){
+      System.out.print("*");
+    }
     
   }
 }
